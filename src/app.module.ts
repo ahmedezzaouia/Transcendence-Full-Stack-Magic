ecb173ab689e9b3babe8caedfbf9be949d7a0fc4
Ahmed Ez zaouia <ahmez-za@c3r10p8.1337.ma>
2023-09-04 14:59:06 +0100

    adding .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+  }),PrismaModule],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
 
-    constructor(){
+    constructor(private config: ConfigService){
         super({
             datasources:{
                 db:{
-                    url:""
+                    url: config.get("DATABASE_URL")
                 }
             }
         })
