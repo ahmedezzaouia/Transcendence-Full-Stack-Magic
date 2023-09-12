9decb5e29cab20474ff5bcf3c4a524f94ad0fa19
Ahmed Ez zaouia <ahmez-za@c3r10p8.1337.ma>
2023-09-12 12:19:39 +0100

    fix import/export between user.module.ts and auth.module.ts

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { JwtStrategy } from 'src/auth/strategies/Jwt.strategy';
-import { JwtService } from '@nestjs/jwt';
+
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  providers: [UserService , JwtStrategy, JwtService],
+  providers: [UserService],
   controllers: [UserController]
 })
 export class UserModule {}
